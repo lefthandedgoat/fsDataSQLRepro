6a5b02362a13710e85d8198a9e2263a47c795969
Chris Holt <lefthandedgoat@gmail.com>
2015-06-23 20:37:05 -0600

    repro

---
+++ repro/Data/sqlQueries.fs
@@ -0,0 +1,27 @@
+module sqlQueries
+
+open FSharp.Data
+
+
+//===============================================================================================
+[<Literal>]
+let reproquery = """
+select count(*) as cnt from
+(
+  select 1 as number
+  union all
+  select 2 as number
+  union all
+  select 3 as number
+) as numbers
+
+"""
+
+type Reproquery = SqlCommandProvider<reproquery, """Server=.\SQLEXPRESS;Integrated Security = true;""">
+
+let runReproquery () =
+  let cmd = new Reproquery()
+  cmd.Execute()
+  |> List.ofSeq
+  |> List.head
+  
